1a0b1d6d55d8edc7323f6a4bf38c8fb096dd6e70
Omar Ayad <omarayad1@gmail.com>
2016-12-26 16:58:39 +0200

    added distribution files

---
+++ dist/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.SNS = undefined;
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _awsSdk = require('aws-sdk');
+
+var _awsSdk2 = _interopRequireDefault(_awsSdk);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var SNS = exports.SNS = function () {
+  function SNS(config) {
+    _classCallCheck(this, SNS);
+
+    this.sns = new _awsSdk2.default.SNS(config);
+  }
+
+  _createClass(SNS, [{
+    key: 'publish',
+    value: function publish(params) {
+      var _this = this;
+
+      return new Promise(function (reject, resolve) {
+        _this.sns.publish(params, function (err, data) {
+          if (err) return reject(err);
+          return resolve(data);
+        });
+      });
+    }
+  }]);
+
+  return SNS;
+}();
